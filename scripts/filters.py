ef64f1823a2322fda20d62e44ff48abd54790efc
Stella Marlene Schultz <s.m.schultz@student.tudelft.nl>
2026-05-29 06:32:11 +0200

    rerun localization to save centroids

--- scripts/filters.py
+++ scripts/filters.py
@@ -4,6 +4,8 @@ no_tissue = [235.61109337, 234.91000469, 233.74045357]
 
 def filter_tissue(patch):
     """Return true if the patch is not tissue, false is the patch is tissue"""
+    if patch.shape[2] == 4:
+        patch = patch[:, :, :3]
     img_mean = np.mean(patch, axis=(0, 1))
     abs_diff = np.abs(np.max(img_mean) - np.min(img_mean))
     return abs_diff < 4.5 or np.allclose(no_tissue, img_mean, rtol=0, atol=4)
